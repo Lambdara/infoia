a6214a0f5160829c57113e8da50d1ad00be54d2a
Uwila <Uwila@users.noreply.github.com>
2018-05-17 14:44:52 +0200

    Cheese

--- src/infoia/CookingAgent.java
+++ src/infoia/CookingAgent.java
@@ -4,15 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import org.semanticweb.HermiT.ReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLOntologyStorageException;
-
+import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 
 public class CookingAgent {
 	public static void main(String[] args) {
@@ -22,18 +22,34 @@ public class CookingAgent {
 	ArrayList<Ingredient> fridge;
 	ArrayList<Recipe> recipeBook;
 	ArrayList<Ingredient> ingredients;
+	OWLOntology ontology;
+	OWLReasoner reasoner;
+	OWLOntologyManager manager;
+	OWLDataFactory dataFactory;
 
 	CookingAgent () {
 		fridge = new ArrayList<Ingredient>();
 		recipeBook = new ArrayList<Recipe>();
 		ingredients = new ArrayList<Ingredient>();
-
-		File folder = new File("recipes/");
+		
+		manager = OWLManager.createOWLOntologyManager();
+        
+        try {
+            String location = "file://" + System.getProperty("user.dir") + "/ontologies/PastaOntology.owl";
+            ontology = manager.loadOntology(IRI.create(location));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        OWLReasonerFactory rf = new ReasonerFactory();
+        reasoner = rf.createReasoner(ontology);
+        dataFactory = manager.getOWLDataFactory();
+		
+		File folder = new File("pasta_recipes/");
 		File[] listOfFiles = folder.listFiles();
 
 		for (File file : listOfFiles) {
 			if (file.isFile()) {
-				System.out.println("Read " + file.getName());
 				String fileName = "recipes/" + file.getName();
 
 				try (Scanner scanner = new Scanner(new File(fileName))) {
@@ -53,31 +69,19 @@ public class CookingAgent {
 						if (ingredient == null) {
 							ingredient = new Ingredient(ingredientName);
 							ingredients.add(ingredient);
-						}
+//						}
 
 						recipe.add(ingredient);
+						}
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-
-		System.out.println(ingredients.size());
-		for(Ingredient i : ingredients) {
-			System.out.println(i.getName());
-		}
-		
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLOntology ontology;
-        try {
-            String location = "file://" + System.getProperty("user.dir") + "/ontologies/IngredientsOntology.owl";
-            System.out.println(location);
-            ontology = manager.loadOntology(IRI.create(location));
-            ontology.saveOntology(new FunctionalSyntaxDocumentFormat(), System.out);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+ 
+		reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+		reasoner.getSubClasses(dataFactory.getOWLClass("http://www.semanticweb.org/jordi/ontologies/2018/4/untitled-ontology-6#Cheese"), false).forEach(System.out::println);;
 	}
 
 	private boolean hasIngredients(Recipe recipe) {
